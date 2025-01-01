0de52e099900fb7a60e7fcc9e582a20d69c5455f
Chinmay Hegde <cod1.1backup@gmail.com>
2025-01-01 16:20:12 +0530

    Created a DB connection file and updated the server.js file to use the connection.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
+import routes from './routes/bookingRoute.js';
+import connectDB from './config/db.js';
 // Configuring the .env file
 dotenv.config();
 
@@ -17,7 +18,11 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.send("Backend");
 })
+
+app.use('/api/booking',routes);
+
 let port = process.env.PORT || 8000  ;
 app.listen(port ,()=>{
+    connectDB();
     console.log(`Server is running on port ${port}`);
 });
